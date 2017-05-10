3d44810f9cd495a65d1d4f96bdcc42c7fcb47798
Dragos Avadanei <dragosav>
2017-05-09 18:36:56 -0700

    fix build warning

--- src/SDKs/KeyVault/dataPlane/Microsoft.Azure.KeyVault.Tests/KeyVaultOperationsTest.cs
+++ src/SDKs/KeyVault/dataPlane/Microsoft.Azure.KeyVault.Tests/KeyVaultOperationsTest.cs
@@ -1351,7 +1351,7 @@ namespace Microsoft.Azure.KeyVault.Tests
                                 .GetAwaiter()
                                 .GetResult();
                         }
-                        catch (KeyVaultErrorException ex)
+                        catch (KeyVaultErrorException)
                         {
                             Assert.False(false, "failed to re-enable disabled secret in soft-delete-enabled vault; cleanup will fail");
                         }
